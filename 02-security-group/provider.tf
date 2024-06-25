4d0af96ea9c03bbcf0eeece35e821ef28890d04e
learndaws <learndaws@gmail.com>
2024-06-25 23:44:24 +0530

    all

--- 02-security-group/provider.tf
+++ 02-security-group/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "daws-common-bucket"
-    key    = "terraform-2-roboshop-project"
+    key    = "terraform-2-roboshop-project-02-security-group"
     region = "us-east-1"
     dynamodb_table = "daws-common-tfstate"
   }
