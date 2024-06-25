240d15eff1088eb8b51598b665dce56935d328c4
learndaws <learndaws@gmail.com>
2024-06-25 19:12:00 +0530

    all

--- 02-security-group/ssm_parameters.tf
+++ 02-security-group/ssm_parameters.tf
@@ -4,6 +4,24 @@ resource "aws_ssm_parameter" "mongodb_sg_id" {
   value = module.mongodb.sg_id
 }
 
+resource "aws_ssm_parameter" "redis_sg_id" {
+  name  = "roboshop_dev_redis_sg_id"
+  type  = "String"
+  value = module.redis.sg_id
+}
+
+resource "aws_ssm_parameter" "mysql_sg_id" {
+  name  = "roboshop_dev_mysql_sg_id"
+  type  = "String"
+  value = module.mysql.sg_id
+}
+
+resource "aws_ssm_parameter" "rabbit_mq_sg_id" {
+  name  = "roboshop_dev_rabbit_mq_sg_id"
+  type  = "String"
+  value = module.rabbit_mq.sg_id
+}
+
 resource "aws_ssm_parameter" "catalogue_sg_id" {
   name  = "roboshop_dev_catalogue_sg_id"
   type  = "String"
@@ -14,4 +32,28 @@ resource "aws_ssm_parameter" "user_sg_id" {
   name  = "roboshop_dev_user_sg_id"
   type  = "String"
   value = module.user.sg_id
+}
+
+resource "aws_ssm_parameter" "cart_sg_id" {
+  name  = "roboshop_dev_cart_sg_id"
+  type  = "String"
+  value = module.cart.sg_id
+}
+
+resource "aws_ssm_parameter" "shipping_sg_id" {
+  name  = "roboshop_dev_shipping_sg_id"
+  type  = "String"
+  value = module.shipping.sg_id
+}
+
+resource "aws_ssm_parameter" "payments_sg_id" {
+  name  = "roboshop_dev_payments_sg_id"
+  type  = "String"
+  value = module.payments.sg_id
+}
+
+resource "aws_ssm_parameter" "web_sg_id" {
+  name  = "roboshop_dev_web_sg_id"
+  type  = "String"
+  value = module.web.sg_id
 }
