a5e52b362d676aebd6c4c77ef8d94617d6d7dbe2
learndaws <learndaws@gmail.com>
2024-06-28 19:32:25 +0530

    all

--- 02-security-group/main.tf
+++ 02-security-group/main.tf
@@ -1,3 +1,73 @@
+module "vpn" {
+    #source = "../../terraform-2-sg-developer"
+    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
+    sg_name = "roboshop_vpn_sg_group"
+    description = "roboshop_vpn_allow-specific"
+    vpc_id = data.aws_vpc.default.id
+    sg_function_tags = var.vpn_sg_function_tags
+    #ingress_rules = var.vpn_ingress_rules
+}
+
+module "web" {
+    #source = "../../terraform-2-sg-developer"
+    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
+    sg_name = "roboshop_web_sg_group"
+    description = "roboshop_web_allow-specific"
+    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
+    sg_function_tags = var.web_sg_function_tags
+    #ingress_rules = var.web_ingress_rules
+}
+
+module "catalogue" {
+    #source = "../../terraform-2-sg-developer"
+    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
+    sg_name = "roboshop_catalogue_sg_group"
+    description = "roboshop_catalogue_allow-specific"
+    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
+    sg_function_tags = var.catalogue_sg_function_tags
+    #ingress_rules = var.catalogue_ingress_rules
+}
+
+module "cart" {
+    #source = "../../terraform-2-sg-developer"
+    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
+    sg_name = "roboshop_cart_sg_group"
+    description = "roboshop_cart_allow-specific"
+    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
+    sg_function_tags = var.cart_sg_function_tags
+    #ingress_rules = var.cart_ingress_rules
+}
+
+module "user" {
+    #source = "../../terraform-2-sg-developer"
+    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
+    sg_name = "roboshop_user_sg_group"
+    description = "roboshop_user_allow-specific"
+    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
+    sg_function_tags = var.user_sg_function_tags
+    #ingress_rules = var.user_ingress_rules
+}
+
+module "shipping" {
+    #source = "../../terraform-2-sg-developer"
+    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
+    sg_name = "roboshop_shipping_sg_group"
+    description = "roboshop_shipping_allow-specific"
+    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
+    sg_function_tags = var.shipping_sg_function_tags
+    #ingress_rules = var.shipping_ingress_rules
+}
+
+module "payments" {
+    #source = "../../terraform-2-sg-developer"
+    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
+    sg_name = "roboshop_payments_sg_group"
+    description = "roboshop_payments_allow-specific"
+    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
+    sg_function_tags = var.payments_sg_function_tags
+    #ingress_rules = var.payments_ingress_rules
+}
+
 module "mongodb" {
     #source = "../../terraform-2-sg-developer"
     source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
@@ -38,74 +108,157 @@ module "rabbit_mq" {
     #ingress_rules = var.rabbit_mq_ingress_rules
 }
 
-module "catalogue" {
-    #source = "../../terraform-2-sg-developer"
-    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
-    sg_name = "roboshop_catalogue_sg_group"
-    description = "roboshop_catalogue_allow-specific"
-    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
-    sg_function_tags = var.catalogue_sg_function_tags
-    #ingress_rules = var.catalogue_ingress_rules
+resource "aws_security_group_rule" "Internet_to_vpn" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks         = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
 }
 
-module "user" {
-    #source = "../../terraform-2-sg-developer"
-    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
-    sg_name = "roboshop_user_sg_group"
-    description = "roboshop_user_allow-specific"
-    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
-    sg_function_tags = var.user_sg_function_tags
-    #ingress_rules = var.user_ingress_rules
+resource "aws_security_group_rule" "vpn_web" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.web.sg_id
 }
 
-module "cart" {
-    #source = "../../terraform-2-sg-developer"
-    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
-    sg_name = "roboshop_cart_sg_group"
-    description = "roboshop_cart_allow-specific"
-    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
-    sg_function_tags = var.cart_sg_function_tags
-    #ingress_rules = var.cart_ingress_rules
+resource "aws_security_group_rule" "internet_web" {
+  cidr_blocks = ["0.0.0.0/0"]
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.web.sg_id
 }
 
-module "shipping" {
-    #source = "../../terraform-2-sg-developer"
-    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
-    sg_name = "roboshop_shipping_sg_group"
-    description = "roboshop_shipping_allow-specific"
-    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
-    sg_function_tags = var.shipping_sg_function_tags
-    #ingress_rules = var.shipping_ingress_rules
+resource "aws_security_group_rule" "vpn_catalogue" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.catalogue.sg_id
 }
 
-module "payments" {
-    #source = "../../terraform-2-sg-developer"
-    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
-    sg_name = "roboshop_payments_sg_group"
-    description = "roboshop_payments_allow-specific"
-    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
-    sg_function_tags = var.payments_sg_function_tags
-    #ingress_rules = var.payments_ingress_rules
+resource "aws_security_group_rule" "web_catalogue" {
+  source_security_group_id = module.web.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.catalogue.sg_id
 }
 
-module "web" {
-    #source = "../../terraform-2-sg-developer"
-    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
-    sg_name = "roboshop_web_sg_group"
-    description = "roboshop_web_allow-specific"
-    vpc_id = data.aws_ssm_parameter.roboshop_dev_vpc_id.value
-    sg_function_tags = var.web_sg_function_tags
-    #ingress_rules = var.web_ingress_rules
+resource "aws_security_group_rule" "cart_catalogue" {
+  source_security_group_id = module.cart.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.catalogue.sg_id
 }
 
-module "vpn" {
-    #source = "../../terraform-2-sg-developer"
-    source = "git::https://github.com/learndaws/terraform-2-sg-developer.git"
-    sg_name = "roboshop_vpn_sg_group"
-    description = "roboshop_vpn_allow-specific"
-    vpc_id = data.aws_vpc.default.id
-    sg_function_tags = var.vpn_sg_function_tags
-    #ingress_rules = var.vpn_ingress_rules
+resource "aws_security_group_rule" "vpn_cart" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
+
+resource "aws_security_group_rule" "web_cart" {
+  source_security_group_id = module.web.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
+
+resource "aws_security_group_rule" "shipping_cart" {
+  source_security_group_id = module.shipping.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
+
+resource "aws_security_group_rule" "payments_cart" {
+  source_security_group_id = module.payments.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_user" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.user.sg_id
+}
+
+resource "aws_security_group_rule" "web_user" {
+  source_security_group_id = module.web.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.user.sg_id
+}
+
+resource "aws_security_group_rule" "payments_user" {
+  source_security_group_id = module.payments.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.user.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_shipping" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.shipping.sg_id
+}
+
+resource "aws_security_group_rule" "web_shipping" {
+  source_security_group_id = module.web.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.shipping.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_payments" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.payments.sg_id
+}
+
+resource "aws_security_group_rule" "web_payments" {
+  source_security_group_id = module.web.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.payments.sg_id
 }
 
 resource "aws_security_group_rule" "catalogue_to_mongodb" {
@@ -153,11 +306,41 @@ resource "aws_security_group_rule" "cart_to_redis" {
   security_group_id = module.redis.sg_id
 }
 
-resource "aws_security_group_rule" "Internet_to_vpn" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "-1"
-  cidr_blocks         = ["0.0.0.0/0"]
-  security_group_id = module.vpn.sg_id
+resource "aws_security_group_rule" "vpn_mysql" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.mysql.sg_id
 }
+
+resource "aws_security_group_rule" "shipping_mysql" {
+  source_security_group_id = module.shipping.sg_id
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = module.mysql.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_rabbit_mq" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.rabbit_mq.sg_id
+}
+
+resource "aws_security_group_rule" "payments_rabbit_mq" {
+  source_security_group_id = module.payments.sg_id
+  type                     = "ingress"
+  from_port                = 5672
+  to_port                  = 5672
+  protocol                 = "tcp"
+  security_group_id        = module.rabbit_mq.sg_id
+}
+
+
+
--- 02-security-group/ssm_parameters.tf
+++ 02-security-group/ssm_parameters.tf
@@ -1,3 +1,45 @@
+resource "aws_ssm_parameter" "vpn_sg_id" {
+  name  = "roboshop_dev_vpn_sg_id"
+  type  = "String"
+  value = module.vpn.sg_id
+}
+
+resource "aws_ssm_parameter" "web_sg_id" {
+  name  = "roboshop_dev_web_sg_id"
+  type  = "String"
+  value = module.web.sg_id
+}
+
+resource "aws_ssm_parameter" "catalogue_sg_id" {
+  name  = "roboshop_dev_catalogue_sg_id"
+  type  = "String"
+  value = module.catalogue.sg_id
+}
+
+resource "aws_ssm_parameter" "cart_sg_id" {
+  name  = "roboshop_dev_cart_sg_id"
+  type  = "String"
+  value = module.cart.sg_id
+}
+
+resource "aws_ssm_parameter" "user_sg_id" {
+  name  = "roboshop_dev_user_sg_id"
+  type  = "String"
+  value = module.user.sg_id
+}
+
+resource "aws_ssm_parameter" "shipping_sg_id" {
+  name  = "roboshop_dev_shipping_sg_id"
+  type  = "String"
+  value = module.shipping.sg_id
+}
+
+resource "aws_ssm_parameter" "payments_sg_id" {
+  name  = "roboshop_dev_payments_sg_id"
+  type  = "String"
+  value = module.payments.sg_id
+}
+
 resource "aws_ssm_parameter" "mongodb_sg_id" {
   name  = "roboshop_dev_mongodb_sg_id"
   type  = "String"
@@ -22,44 +64,8 @@ resource "aws_ssm_parameter" "rabbit_mq_sg_id" {
   value = module.rabbit_mq.sg_id
 }
 
-resource "aws_ssm_parameter" "catalogue_sg_id" {
-  name  = "roboshop_dev_catalogue_sg_id"
-  type  = "String"
-  value = module.catalogue.sg_id
-}
 
-resource "aws_ssm_parameter" "user_sg_id" {
-  name  = "roboshop_dev_user_sg_id"
-  type  = "String"
-  value = module.user.sg_id
-}
 
-resource "aws_ssm_parameter" "cart_sg_id" {
-  name  = "roboshop_dev_cart_sg_id"
-  type  = "String"
-  value = module.cart.sg_id
-}
 
-resource "aws_ssm_parameter" "shipping_sg_id" {
-  name  = "roboshop_dev_shipping_sg_id"
-  type  = "String"
-  value = module.shipping.sg_id
-}
 
-resource "aws_ssm_parameter" "payments_sg_id" {
-  name  = "roboshop_dev_payments_sg_id"
-  type  = "String"
-  value = module.payments.sg_id
-}
 
-resource "aws_ssm_parameter" "web_sg_id" {
-  name  = "roboshop_dev_web_sg_id"
-  type  = "String"
-  value = module.web.sg_id
-}
-
-resource "aws_ssm_parameter" "vpn_sg_id" {
-  name  = "roboshop_dev_vpn_sg_id"
-  type  = "String"
-  value = module.vpn.sg_id
-}
